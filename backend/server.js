de6218ffc142f247c6b796d38c163607eef3435f
Sabrina <sabrinawhitee@yahoo.com>
2021-09-10 10:02:10 -0700

    Add api fuctionality to get commit info from github server and return json with info

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,48 @@
 const express = require("express");
 const app = express();
+const { Octokit } = require("@octokit/core");
+const octokit = new Octokit({ auth: `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` });
 
-app.get('/search', (req, res) => {
-    const {url} = req.query;
-    console.log(req)
-    res.send(`<h1>Search results for: ${url}</h1>`);
+
+app.get('/search', async (req, res) => {
+    try {
+        const { url } = req.query;
+        const url_arr = url.split('/');
+        const repo = url_arr[url_arr.length - 1];
+        const user = url_arr[url_arr.length - 2];
+
+        // Get all of the open pull requests
+        const request = await octokit.request('GET /repos/{owner}/{repo}/pulls', {
+            owner: user,
+            repo: repo,
+            state: "open"
+        })
+
+        const prs = request.data.map(pr => pr.number);
+        const return_json = {
+            "pull_requests": [
+            ]
+        }
+        // For each pull request, get the number of commits
+        for (let pr of prs) {
+            const request = await octokit.request('GET /repos/{owner}/{repo}/pulls/{pull_number}/commits', {
+                owner: user,
+                repo: repo,
+                pull_number: pr
+            })
+            // Add the pull request number and commit length to the return json
+            const pull_request = { "pr_number": pr, "num_of_commits": request.data.length }
+            return_json.pull_requests.push(pull_request);
+        }
+
+        res.json(return_json);
+    }
+    // in the event of an error, return an error page
+    catch(e){
+        res.send(`<h1>Error trying to access ${req.query.url}. Returned with error: ${e}</h1>`)
+    }
 })
 
-app.listen(3000, ()=> {
+app.listen(3000, () => {
     console.log("Listening on port 3000")
 })
